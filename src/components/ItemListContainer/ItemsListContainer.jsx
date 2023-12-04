31bbb43f59046c3e3da6f381d151a887681187fc
Juan Pablo Portillo <juanp.portillob@gmail.com>
2023-12-04 16:06:32 -0600

    primera entrega terminada

---
+++ src/components/ItemListContainer/ItemsListContainer.jsx
@@ -0,0 +1,14 @@
+const ItemListContainer = ({greeting}) => {
+
+  return (
+
+    <div className="text-center">
+      <h1>Bienvenidos</h1>
+      <h5>{greeting}</h5>
+    </div>
+
+  );
+
+};
+
+export default ItemListContainer;
---
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import CartWidgetComponent from '../CartWidgetComponent/CartWidget';
+
+import "./Navbar.css";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div className="container">
+        <p>
+          <a href="https://github.com/JuanP-Portillo/React-Coderhouse" className="navInfo">
+            Github: JuanP-Portillo
+          </a>{' '}
+          |{' '}
+          <a
+            href="mailto:juanp.portillob@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="navInfo"
+          >
+            juanp.portillob@gmail.com
+          </a>
+        </p>
+      </div>
+
+      <div>
+        <Navbar expand="lg" bg="light" variant="light" className="me-navbar-style">
+          <Container fluid>
+
+            <Navbar.Brand href="#">
+              <img
+                src="./src/assets/images/rocket.png"
+                alt="Logo"
+                className="d-inline-block align-top logo"
+                style={{ width: '50px', height: '50px' }}
+              />
+            </Navbar.Brand>
+
+
+            <Navbar.Toggle aria-controls="navbar-nav" />
+
+            <Navbar.Collapse id="navbar-nav">
+
+              <Nav className="me-auto gap-5 me-navBar-style">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#productos">Productos</Nav.Link>
+                <Nav.Link href="#ofertas">Ofertas</Nav.Link>
+                <Nav.Link href="#galeria">Galeria</Nav.Link>
+                <Nav.Link href="#contacto">Contacto</Nav.Link>
+              </Nav>
+
+
+              <div className="d-flex align-items-center">
+                <CartWidgetComponent />
+              </div>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
